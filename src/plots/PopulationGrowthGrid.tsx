8bdd6ad4b5a8d78ea9d72ea9bb491d4e951a59a4
itsparasbisht <iamparasbisht@gmail.com>
2024-11-23 14:47:45 +0530

    growth rate observations card added

--- src/plots/PopulationGrowthGrid.tsx
+++ src/plots/PopulationGrowthGrid.tsx
@@ -1,4 +1,16 @@
 import { useEffect, useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../components/ui/tooltip";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "../components/ui/hover-card";
+import { InfoIcon } from "lucide-react";
 
 type PopulationGrowth = {
   year: number;
@@ -25,28 +37,63 @@ export default function PopulationGrowthGrid({ data }: WorldPopulation) {
   console.log(populationGrowthRate);
 
   return (
-    <div className="p-4 pt-0">
-      <h2 className="text-xl font-semibold">
-        Population growth rate over the years{" "}
-        <span className="text-lg font-normal text-red-600">
-          (hover over grid for growth rate)
-        </span>
-      </h2>
-      <div className="flex p-4">
+    <div className="p-4 pt-0 w-full">
+      <div className="flex justify-between">
+        <h2 className="text-xl font-semibold">
+          Population growth rate over the years{" "}
+          <span className="text-sm font-bold text-red-600">
+            (hover over grid for growth rate)
+          </span>
+        </h2>
+        <div>
+          <HoverCard openDelay={300}>
+            <HoverCardTrigger className="flex cursor-pointer bg-gray-900 p-2 gap-1 rounded-sm text-white">
+              <InfoIcon /> Observations
+            </HoverCardTrigger>
+            <HoverCardContent className="w-[500px] bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
+              <div>
+                <p className="m-2">
+                  - The highest growth rates occurred during the early 1960s to
+                  1970, peaking at 2.13% in 1963.
+                </p>
+                <p className="m-2">
+                  - After the 1960s, the population growth rate shows a
+                  consistent decline, from 2.13% in 1963 to 0.92% in 2023.
+                </p>
+                <p className="m-2">
+                  - In the early 21st century (2000–2023), growth rates
+                  stabilized around 1.2%, followed by a sharper decline
+                  post-2015.
+                </p>
+                <p className="m-2">
+                  - The growth rate fell below 1% for the first time in 2021
+                  (0.87%).
+                </p>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-y-2 p-4">
         {populationGrowthRate.map((item) => (
-          <div
-            key={item.year}
-            className="w-5 h-12"
-            style={{ backgroundColor: getColorByGrowthRate(item.growth_rate) }}
-            title={`Year: ${item.year}, Growth Rate: ${
-              item.growth_rate ?? "N/A"
-            }%`}
-          >
-            {/* Accessible content for screen readers */}
-            <span className="sr-only">{`Year: ${item.year}, Growth Rate: ${
-              item.growth_rate ?? "N/A"
-            }%`}</span>
-          </div>
+          <TooltipProvider key={item.year} delayDuration={100}>
+            <Tooltip>
+              <TooltipTrigger>
+                <div
+                  key={item.year}
+                  className="flex-1 w-5 h-12"
+                  style={{
+                    backgroundColor: getColorByGrowthRate(item.growth_rate),
+                  }}
+                ></div>
+              </TooltipTrigger>
+              <TooltipContent>
+                {`Year: ${item.year}, Growth Rate: ${
+                  item.growth_rate ?? "N/A"
+                }%`}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         ))}
       </div>
     </div>
@@ -75,7 +122,7 @@ function provideGrowthRate(data: { [key: string]: number }) {
 }
 
 function getColorByGrowthRate(growthRate: number | null) {
-  if (growthRate === null) return "bg-gray-300";
+  if (growthRate === null) return "gray";
   const intensity = Math.min(Math.max(growthRate, 0), 5) * 180;
   const r = Math.min(255 - intensity);
   const g = Math.min(255 - intensity / 1.8);
