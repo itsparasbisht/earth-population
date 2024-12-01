cfbc7dff4fbf6c4015c5b7ee4e8418301932040f
itsparasbisht <iamparasbisht@gmail.com>
2024-12-01 20:14:24 +0530

    world fertility observations added

--- src/plots/WorldFertility.tsx
+++ src/plots/WorldFertility.tsx
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import worldFertilityData from "../../data/world-fertility.json";
 import * as echarts from "echarts";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../components/ui/popover";
+import { InfoIcon } from "lucide-react";
 
 type FertilityEntry = {
   year: number;
@@ -16,8 +22,8 @@ export default function WorldFertility() {
 
   return (
     <div className="w-full h-full">
-      <div>
-        <p className="p-2">
+      <div className="flex items-center p-2">
+        <p>
           <b className="text-2xl">Fertility, </b>
           the average number of children born to a woman during her reproductive
           years (typically ages 15–49). Fertility is a key indicator used to
@@ -25,6 +31,34 @@ export default function WorldFertility() {
           factors such as economic development, health policies, and resource
           planning.
         </p>
+        <div>
+          <Popover>
+            <PopoverTrigger className="flex gap-2 bg-gray-900 text-white p-2 rounded-sm hover:bg-gray-700">
+              <InfoIcon />
+              Observations
+            </PopoverTrigger>
+            <PopoverContent className="w-[600px] bg-gray-800 text-white border-0">
+              <p className="m-2">
+                - From 1960 to 2022, the fertility rate shows a significant
+                overall decline from 4.70 in 1960 to 2.26 in 2022, indicating a
+                shift towards smaller family sizes globally.
+              </p>
+              <p className="m-2">
+                - Fertility peaked at 5.32 in 1963 before gradually starting to
+                decline.
+              </p>
+              <p className="m-2">
+                - By 2022, the global fertility rate (2.26) is approaching the
+                replacement-level fertility rate of 2.1, which is the level
+                needed for a stable population without immigration.
+              </p>
+              <p className="m-2">
+                - This trend suggests population growth may slow or decline in
+                many regions.
+              </p>
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
       <div
         id="world-fertility"
