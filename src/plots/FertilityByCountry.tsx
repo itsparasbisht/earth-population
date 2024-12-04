1cf18c2ec9d3b76f68b8055f90b2cd1f372152e5
itsparasbisht <iamparasbisht@gmail.com>
2024-12-04 20:52:55 +0530

    prev

--- src/plots/FertilityByCountry.tsx
+++ src/plots/FertilityByCountry.tsx
@@ -1,6 +1,7 @@
 import { FertilityEntry } from "@/functions/getFertilityData";
 import * as echarts from "echarts";
 import { useEffect } from "react";
+import fertilityLevels from "../../data/fertility-levels.json";
 
 type FertilityByCountryProps = {
   data: FertilityEntry[];
@@ -15,6 +16,31 @@ export default function FertilityByCountry({ data }: FertilityByCountryProps) {
 
   return (
     <div className="w-full h-full">
+      <div className="flex flex-wrap p-4 gap-4">
+        {fertilityLevels.map((item) => (
+          <div
+            className="w-[400px] flex-col items-center justify-center p-4 rounded-lg"
+            style={{
+              backgroundColor: item["bg-color"],
+              color: item["text-color"],
+            }}
+          >
+            <div className="font-bold">
+              <span className="text-2xl">{item.rate}</span>
+              <span className="text-xl ml-2">({item.title})</span>
+            </div>
+            <div
+              className="w-full h-[1px] mt-2"
+              style={{ backgroundColor: item["text-color"] }}
+            ></div>
+            <ul className="px-6 py-3 list-disc">
+              {item.pointers.map((point) => (
+                <li className="mb-1">{point}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
       <div
         id="fertility-by-country"
         className="w-full min-w-[600px] h-[85%] max-h-[800px] p-4"
